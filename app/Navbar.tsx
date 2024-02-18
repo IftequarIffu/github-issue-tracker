012376a0795f87f5a07397f8f72ab1bf22c47af7
Iftequar Ahmed <iftequar.154@gmail.com>
2024-02-18 13:39:00 +0530

    Done with Prisma setup

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,37 +1,41 @@
-'use client';
+"use client";
+// Because usePathname uses client-side APIs
 
-import Link from 'next/link'
-import React from 'react'
-import { AiFillBug } from 'react-icons/ai'
-import { usePathname } from 'next/navigation'
-import classnames from 'classnames'
+import Link from "next/link";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const Navbar = () => {
+  const currentPath = usePathname();
 
-    const currentPath = usePathname();
-
-    const links = [
-        {label: 'Dashboard', href: '/'},
-        {label: 'Issues', href: '/issues'}
-    ]
-
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
 
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/" className='me-auto'><AiFillBug /></Link>
-        <ul className='flex space-x-6'>
-            {
-                links.map((link) => (
-                    <li key={link.href} className={classnames({
-                        'text-zinc-900' : link.href === currentPath,
-                        'text-zinc-500' : link.href !== currentPath,
-                        'hover:text-zinc-800 transition-colors': true
-                    })}><Link href={link.href}>{link.label}</Link></li>
-                ))
-            }
-        </ul>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/" className="me-auto">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li
+            key={link.href}
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+          >
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
