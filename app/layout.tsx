532be6ffd4ca11e4122578c94cd3eb5ac6fb2fcd
Iftequar Ahmed <iftequar.154@gmail.com>
2024-02-22 11:53:54 +0530

    Added Inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,14 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './Navbar'
-import { Theme } from '@radix-ui/themes';
+import { Theme, ThemePanel } from '@radix-ui/themes';
+import './theme-config.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter(
+  { 
+    subsets: ['latin'],
+    variable:  '--font-inter'
+  })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,8 +24,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+      <body className={inter.variable}>
+        
+        <Theme appearance="light" accentColor="violet">
           <Navbar />
             <main className='p-5'>
               {children}
