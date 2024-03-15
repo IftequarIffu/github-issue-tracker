729e1fc0bd1916da1c5a49f57b1c682000dc87e7
Iftequar Ahmed <iftequar.154@gmail.com>
2024-03-15 11:05:13 +0530

    Built an API for deleting Issues

--- app/api/issues/[issueId]/route.tsx
+++ app/api/issues/[issueId]/route.tsx
@@ -49,3 +49,24 @@ export const PUT = async(request: NextRequest, { params }: { params: {issueId: s
 
     return NextResponse.json(updatedIssue, {status: 200})
 }
+
+export const DELETE = async(request: NextRequest, { params }: { params: {issueId: string}}) => {
+
+    const issue = await prisma.issue.findUnique({
+        where: {
+            id: parseInt(params.issueId)
+        }
+    })
+
+    if(!issue){
+        return NextResponse.json({error: "Invalid Issue"}, {status: 404})
+    }
+
+    const deletedIssue = await prisma.issue.delete({
+        where: {
+            id: issue.id
+        }
+    })
+
+    return NextResponse.json({}, {status: 200})
+}
