773af84c386f0da8e53d812d5939780ed9ec2e5f
Iftequar Ahmed <iftequar.154@gmail.com>
2024-04-10 16:31:50 +0530

    Implemented pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Pagination from './components/Pagination'
 
-const page = () => {
+const Home = ({ searchParams }: { searchParams : { page : string }}) => {
   return (
-    <Pagination itemCount={100} pageSize={10} currentPage={2}/>
+    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}/>
     // <Pagination issues={issues} issuesPerPage={10}/>
     // <h1>Abcd</h1>
   )
 }
 
-export default page
+export default Home
